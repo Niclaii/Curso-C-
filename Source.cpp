c1a30f98e908a61778340e5722f4cb2040b6e22a
Nicolas Quintanilla <107016019+Niclaii@users.noreply.github.com>
2023-01-30 11:46:23 -0500

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -53,3 +53,41 @@ int main() {
 
     return 0;
 }
+
+
+
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+  int opt{1};
+  cout << "Elija una opcion \n[1] Minimo comun Multiplo. \n [2] Maximo comun divisor. \n >>";
+  cin >> opt;
+
+  if (opt==1){
+
+    int a{-1};
+    cout << "ingrese un numero: ";
+    cin >> a
+
+    int b{2};
+    cout << "minimo comun multiplo = ";
+    while (a != 1 ){
+      if ((a%b)==0){
+
+        a=a/b;
+
+        cout <<b <<"x";
+
+      }
+      else{
+        b++
+      }
+    }
+  }
+
+
+
+  return 0;
+}*/
